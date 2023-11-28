db080ff425a197cd982bc2299c70fa0b817a5fb0
Thiago Marim <thiago.marim2005@gmail.com>
2023-11-28 17:05:40 -0300

    criacao de modules

---
+++ js/modules/menuMobile.js
@@ -0,0 +1,12 @@
+export default function initMenuMobile() {
+  const menuMobile = document.querySelector('.menu-hamburguer');
+  const menuItems = document.querySelector('.header-menu-items');
+  const eventos = ['click', 'touchstart'];
+
+  function toggleMenu() {
+    menuMobile.classList.toggle('ativo');
+    menuItems.classList.toggle('ativo');
+  }
+
+  eventos.forEach((evento) => menuMobile.addEventListener(evento, toggleMenu));
+}
